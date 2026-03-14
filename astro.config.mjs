0ae3e552dc4ce64ae62d8878f7c3970f04d106e1
Joseph Hill <josephh310@icloud.com>
2026-03-14 22:53:42 +0000

    chore: added home url to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://josephh.dev/',
 	integrations: [mdx(), sitemap()],
 });
